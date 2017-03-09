5da2b96d77fdcec08d4a52b3ef3b8cfebde1602a
jim.xu <jim.xu@memeboxglobal.com>
2017-03-09 11:57:09 +0800

    # init

---
+++ two_sum.js
@@ -0,0 +1,26 @@
+/*
+* @Author: jim
+* @Date:   2017-03-09 11:07:57
+* @Last Modified by:   jim
+* @Last Modified time: 2017-03-09 11:16:35
+*/
+
+'use strict';
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    return nums;
+};
+
+var nums = [2,7,11,15];
+var target = 9;
+console.log(twoSum(nums,target));
